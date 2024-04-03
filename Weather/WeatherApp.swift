9ca58d3e37a0224c7fdadf6054f8054749d9f8f3
Febry Ardiansyah <febrymuhammad80@gmail.com>
2024-04-03 16:27:04 +0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Febry Ardiansyah on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
